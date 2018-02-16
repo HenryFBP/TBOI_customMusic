b7cb1594faada5680e14b5e2c19afaaa55272a13
Henry Post <HenryFBP@gmail.com>
2018-02-16 10:01:30 -0600

    add HTTP test as TBOI LUA API does not allow adding new sounds or music

--- main.lua
+++ main.lua
@@ -1,19 +1,38 @@
-StartDebug()
-
+-- StartDebug()
 local Mod = RegisterMod("CustomMusic", 1)
 
-Isaac.DebugString("custom music mod?? hi!")
+local httpsocket = require("socket.http")
+--local http = require('http')
+
+local config = {
+  ["XPos"]="50",
+  ["YPos"]="30",
+
+}
+
+--http.createServer(function (req, res)
+--  local body = "Hello world\n"
+--  res:setHeader("Content-Type", "text/plain")
+--  res:setHeader("Content-Length", #body)
+--  res:finish(body)
+--end):listen(1337, '127.0.0.1')
+
+--print('Server running at httcvp://127.0.0.1:1337/')
 
 
 function Mod:onRender()
     local x = Isaac.GetPlayer(0).Position.X
     local y = Isaac.GetPlayer(0).Position.Y
-    Isaac.RenderText("hi. x="..x..",y="..y, x, y, 255, 255, 255, 255)
+    
+    local s = "hi. x="..x..",y="..y
+    
+    Isaac.RenderText(s, x, y, 255, 255, 255, 255)
 end
 
 function Mod:immortality()
     local p = Isaac.GetPlayer(0)
     p:SetFullHearts()
+    local body, code, headers, status = http.request("https://localhost:8080")
 end
 
 
