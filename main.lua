34d6081fbbae6d806dc7c8ed947600709b730cd0
Henry Post <HenryFBP@gmail.com>
2018-02-17 14:23:25 -0600

    finalize room-checking code to only send if we change room types

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
 --StartDebug()
 
+_debug = true
+
 function table.val_to_str ( v )
   if "string" == type( v ) then
     v = string.gsub( v, "\n", "\\n" )
@@ -36,34 +38,34 @@ function table.tostring( tbl )
   return "{" .. table.concat( result, "," ) .. "}"
 end
 
+function table.invert(t)
+   local s={}
+   for k,v in pairs(t) do
+     s[v]=k
+   end
+   return s
+end
+
 function script_path()
    local str = debug.getinfo(2, "S").source:sub(2)
    return str:match("(.*/)")
 end
-
-
-function os.capture(cmd, raw)
-  local f = assert(io.popen(cmd, 'r'))
-  local s = assert(f:read('*a'))
-  f:close()
-  if raw then return s end
-  s = string.gsub(s, '^%s+', '')
-  s = string.gsub(s, '%s+$', '')
-  s = string.gsub(s, '[\n\r]+', ' ')
-  return s
-end
-
 
 local _name = 'CustomMusic'
 local Mod = RegisterMod(_name, 1)
 local mainModule = {}
 _log = {}
 
+for i=1, 15 do
+  _log[i] = "..."
+end
+
 local _path = script_path()
 local _script = 'py'
 local _scriptMessenger = 'messenger.py'
 local _scriptServer = 'web/manage.py'
 local _ticks = 0
+lastRoomID = nil
 
 --local serverLog = start()
 lastSent = 'oh helo'
@@ -77,31 +79,15 @@ local config = {
 
 function PrintText(thing, x, y)
   x = x or config['XPos']
-  y = y or config['YPos']+50
+  y = y or config['YPos']+100
   
   Isaac.RenderText(thing, x, y, 255, 255, 255, 255)
 end
 
 function Log(thing, place)
-        
-  x = config['XPos']
-  y = config['YPos']+50
-  
+
   _log[place] = thing
 
-end
-
-
-
-function Mod:start()
-  local command = 'cmd /K '.._script..' "'.._path.._scriptServer..'" runserver'
-  os.execute(command)
-  return command
-end
-
-
-function test()
-  Log('Testing?????',10)
 end
 
 --- sendCMD sends a command and opens a command-line window.
@@ -111,7 +97,7 @@ end
 -- @return The message that was sent.
 function sendCMD(message, stay)
   stay = stay or false
-  local command = 'py "'.._path.._scriptMessenger..'"'
+  local command = 'py "'.._path.._scriptMessenger..'" '..message
   
   if stay then
     command = command .. " & PAUSE"
@@ -125,8 +111,10 @@ end
 -- @param message the message to be sent to the customMusic server.
 -- @return The 
 function send(message)
-  local command = 'py "'.._path.._scriptMessenger..'"'
-  local out = os.capture(command)
+  local command = 'py "'.._path.._scriptMessenger..'" '..message
+
+  local out = os.execute(command)
+
   return out
 end
 
@@ -146,6 +134,22 @@ function displayLog()
     
   end
   
+end
+
+function getkeys(t)
+  local klst = {}
+  local i = 1
+  
+  for key, value in pairs(t) do
+    klst[i] = key
+    i = i + 1
+  end
+  
+  return k
+end
+
+function time()
+  return "["..os.clock().."]:"
 end
 
 function Mod:onRender()
@@ -169,10 +173,53 @@ function Mod:immortality()
   local p = Isaac.GetPlayer(0)
   p:SetFullHearts()
   
-  lastSent = "last sent:" .. sendCMD("hello webserver!", true)
+  Log(time().." got hurt :'(",2)
+  
+--  lastSent = "last sent:" .. send("OW!!", true) -- to test our sending features
+  
+  
 end
 
+--- Called to tell the server we changed rooms.
+function Mod:roomchange()
+  local i = 6
+  
+  local room = Game():GetRoom()
+  local RoomID = room:GetType()
+  local typename = table.invert(RoomType)[RoomID]
+  
+    if lastRoomID == nil then -- if this is the first room we've been to
+    lastRoomID = RoomID
+  end
+  
+  Log(time().."Room type we just entered: "..RoomID.." aka "..table.invert(RoomType)[RoomID],i)
+  i=i+1
+  
+  Log("Comparing '"..lastRoomID.."' and '"..RoomID..'".',i)
+  i=i+1
+  
+  if lastRoomID ~= RoomID then -- we should change music if we are in a different room
+    
+    send(typename, true) -- tell the server we've changed rooms
+    
+    Log("Sending that we've changed rooms!", i)
+    i=i+1
+  
+  else
+    Log("Changed rooms but not the room type. Not sending.",i)
+    i=i+1
+  end
+  
+  lastRoomID = RoomID -- record what we just changed to
+  
+end
+
+if debug == false then -- disable logging if debug mode is off
+  Log = function(thing) end
+end
 
 Mod:AddCallback(ModCallbacks.MC_POST_RENDER, Mod.onRender)
 Mod:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, Mod.immortality, EntityType.ENTITY_PLAYER)
 Mod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, Mod.start, EntityType.ENTITY_PLAYER)
+Mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, Mod.roomchange, EntityType.ENTITY_PLAYER)
+--Mod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, Mod.levelchange, EntityType.ENTITY_PLAYER)
