4bdef59958a95b6daf1312b571dc71261063a858
Henry Post <HenryFBP@gmail.com>
2018-02-16 21:49:47 -0600

    functional python-invoking code!

--- main.lua
+++ main.lua
@@ -1,40 +1,34 @@
--- StartDebug()
-local Mod = RegisterMod("CustomMusic", 1)
+StartDebug()
 
-local httpsocket = require("socket.http")
---local http = require('http')
+local _name = "CustomMusic"
+local Mod = RegisterMod(_name, 1)
+
+local testModule = require('testModule')
 
 local config = {
-  ["XPos"]="50",
-  ["YPos"]="30",
-
+  ["XPos"]=50,
+  ["YPos"]=30,
+  ["LineHeight"]=10,
 }
 
---http.createServer(function (req, res)
---  local body = "Hello world\n"
---  res:setHeader("Content-Type", "text/plain")
---  res:setHeader("Content-Length", #body)
---  res:finish(body)
---end):listen(1337, '127.0.0.1')
-
---print('Server running at httcvp://127.0.0.1:1337/')
-
-
 function Mod:onRender()
-    local x = Isaac.GetPlayer(0).Position.X
-    local y = Isaac.GetPlayer(0).Position.Y
-    
-    local s = "hi. x="..x..",y="..y
-    
-    Isaac.RenderText(s, x, y, 255, 255, 255, 255)
+
+  local x = Isaac.GetPlayer(0).Position.X
+  local y = Isaac.GetPlayer(0).Position.Y
+
+  local s = _name..". x="..x..",y="..y
+
+  Isaac.RenderText(s, config["XPos"], config["YPos"], 255, 255, 255, 255)
+  Isaac.RenderText(testModule.test(), config["XPos"], config["YPos"]+(1 * config["LineHeight"]), 255, 255, 255, 255)
 end
 
 function Mod:immortality()
-    local p = Isaac.GetPlayer(0)
-    p:SetFullHearts()
-    local body, code, headers, status = http.request("https://localhost:8080")
+  testModule.send("hello webserver!")
+  local p = Isaac.GetPlayer(0)
+  p:SetFullHearts()
+--    local body, code, headers, status = http.request("https://localhost:8080")
 end
 
 
 Mod:AddCallback(ModCallbacks.MC_POST_RENDER, Mod.onRender)
--- Mod:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, Mod.immortality, EntityType.ENTITY_PLAYER)
+Mod:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, Mod.immortality, EntityType.ENTITY_PLAYER)
---
+++ test.py
@@ -0,0 +1,13 @@
+def main():
+    s = ""
+    
+    while True:
+        s = input("Enter 'q' to quit.")
+
+        if 'quit' in s:
+            break
+
+        print("hi mom???")
+
+if __name__ == '__main__':
+    main()
