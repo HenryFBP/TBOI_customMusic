bc57f8affde653b5c2d70bf63ac718edddae3435
HenryFBP <HenryFBP@gmail.com>
2018-02-27 18:32:25 -0600

    separate python settings

---
+++ settings.py
@@ -0,0 +1,19 @@
+import sys
+import os
+
+debug = False
+
+os_type = sys.platform
+mod_name = 'CustomMusic'
+runtime = 'python'
+
+host = 'localhost'
+port = 8000
+
+messages_path = "/messages"
+messages_file = "msg.log"
+
+server_script = '/web/manage.py'
+script_path = os.path.dirname(sys.argv[0])
+
+message = {"defaultsettings": "message"}
